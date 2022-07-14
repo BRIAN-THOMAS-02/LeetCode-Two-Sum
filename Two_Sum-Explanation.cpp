3d38f8d8ca565c96d85c8975ec501725cd902e6d
BRIAN THOMAS <87309254+BRIAN-THOMAS-02@users.noreply.github.com>
2022-07-14 23:37:33 +0530

    Create Two_Sum-Explanation.cpp

---
+++ Two_Sum-Explanation.cpp
@@ -0,0 +1,60 @@
+LeetCode - Two Sum Explanation : https://leetcode.com/problems/two-sum/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution 
+{
+    
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        cout<<"target : "<<target<<"\n\n";
+        vector<int> a;
+        vector<int> b;
+        int n = nums.size();
+        
+        cout<<"n : "<<n;
+        for (int i=0; i<n-1; i++)
+        {
+            cout<<"\n\ni : "<<i<<"\n";
+            for (int j=i+1; j<n; j++)
+            {
+                cout<<"  ";
+                cout<<nums[i]<<" + "<<nums[j]<<" = "<<nums[i] + nums[j]<<"  |  target : "<<target<<"\n";
+                if (nums[i] + nums[j] == target)
+                {
+                    cout<<"  ANS MILGAYA BOIZZZ\n\n";
+                    a.push_back(i);
+                    a.push_back(j);
+                }
+            }
+        }
+        
+        cout<<"\n\n";
+        cout<<"a.size() : "<<a.size();
+        
+        int l;
+        cout<<"\n";
+        for (l=0; l<a.size(); l++)
+        {
+            cout<<"\nl : "<<l;
+            cout<<"  | a["<<l<<"] : "<<a[l];
+        }
+        
+    return a;
+    }
+};
+
+
+int main()
+{   
+    vector<int> vector_1 = {1,1,1,2,1,4,8,9};
+    Solution obj;
+    obj.twoSum(vector_1, 6);
+    
+    /*vector<int> input;
+    input = obj.twoSum(vector_1, 9);
+    cout<<input;*/
+}
+
